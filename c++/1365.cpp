cc1846587eccbe959492f1cd52f63372875c6d9a
Devin Rucks <blindnives@gmail.com>
2020-05-07 11:25:51 -0400

    first commit

---
+++ c++/1365.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
+// That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+// Return the answer in an array.
+
+// Input: nums = [8,1,2,2,3]
+// Output: [4,0,1,1,3]
+// Explanation:
+// For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3).
+// For nums[1]=1 does not exist any smaller number than it.
+// For nums[2]=2 there exist one smaller number than it (1).
+// For nums[3]=2 there exist one smaller number than it (1).
+// For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+
+class Solution
+{
+public:
+     vector<int> smallerNumbersThanCurrent(vector<int> &nums)
+     {
+          int arrayLen = nums.size();
+          int currentNum;
+          vector<int> res(arrayLen);
+
+          for (int i = 0; i < arrayLen; i++)
+          {
+               currentNum = nums[i];
+
+               for (int j = 0; j < arrayLen; j++)
+               {
+                    if (currentNum < nums[j])
+                    {
+                         res[j] += 1;
+                    }
+               }
+          }
+          return res;
+     };
+};
